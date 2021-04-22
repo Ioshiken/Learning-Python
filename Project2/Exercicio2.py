a38ee21ae8fccfb89ceb312e199c291fac272179
Celso Ohphata <68305610+Ioshiken@users.noreply.github.com>
2021-04-21 23:28:10 -0300

    Exercicio de operadores aritmeticos

---
+++ Project2/Exercicio2.py
@@ -0,0 +1,18 @@
+n1= int(input('Um valor: '))
+n2= int(input('Outro Valor: '))
+s = n1 + n2
+m = n1 * n2
+d = n1 / n2
+di = n1 // n2
+e = n1 ** n2
+print('A soma e {},\n O produto e {} e \n A divisao e {:.2f}'.format(s, m, d),end=' ')
+print('Divisao inteira {} e potencia {}'.format(di, e))
+print( 'A soma vale {}'.format(n1+n2))
+
+
+
+
+
+
+
+
